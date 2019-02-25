5e749f836f025345df83af0cb36604659ce2cb99
zck <chuankunzhang@mjitech.com>
2019-02-25 14:58:53 +0800

    提交 history

---
+++ spark_2.2_hadoop_2.6.5/spark-env.sh
@@ -0,0 +1,2 @@
+export SPARK_HISTORY_OPTS="-Dspark.history.ui.port=18080 -Dspark.history.retainedApplications=3 -Dspark.history.fs.logDirectory=hdfs://localhost:9000/history
+export HADOOP_CONF_DIR=/usr/local/hadoop-2.6.5/etc/hadoop
