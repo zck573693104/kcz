ed45dfb3023f32ade37204da7bdaae5e9c939ac4
zck <chuankunzhang@mjitech.com>
2019-03-04 19:38:20 +0800

    kafka

--- spark_2.2_hadoop_2.6.5/initRun.sh
+++ spark_2.2_hadoop_2.6.5/initRun.sh
@@ -4,3 +4,5 @@
 hadoop fs -mkdir -p /history
 hadoop fs -mkdir -p /spark/history
 /usr/local/hadoop-2.6.5/sbin/start-yarn.sh
+/usr/local/zookeeper-3.4.13/bin/zkServer.sh start
+/usr/local/kafka_2.11-2.1.0/bin/kafka-server-start.sh /usr/local/kafka_2.11-2.1.0/config/server.properties
