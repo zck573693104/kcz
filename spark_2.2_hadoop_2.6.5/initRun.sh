5c30ec92e81cc91468a30bd9d4c6686ad5f89a59
zck <chuankunzhang@mjitech.com>
2019-03-27 19:43:01 +0800

    提交 hbase

--- spark_2.2_hadoop_2.6.5/initRun.sh
+++ spark_2.2_hadoop_2.6.5/initRun.sh
@@ -4,10 +4,12 @@
 hadoop fs -mkdir -p /history
 hadoop fs -mkdir -p /spark/history
 hadoop fs -mkdir /tmp
+hadoop fs -mkdir /hbase
 hadoop fs -mkdir -p /usr/hive/warehouse
 hadoop fs -chmod g+w /tmp
 hadoop fs -chmod g+w /usr/hive/warehouse
 /usr/local/hadoop-2.6.5/sbin/start-yarn.sh
 /usr/local/zookeeper-3.4.13/bin/zkServer.sh start
 /usr/local/mysql.sh
+schematool -dbType mysql -initSchema
 /usr/local/kafka_2.11-2.1.0/bin/kafka-server-start.sh /usr/local/kafka_2.11-2.1.0/config/server.properties
