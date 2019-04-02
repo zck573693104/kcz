b3372aff5f400cbd591b710271af5d8760d314be
zck <chuankunzhang@mjitech.com>
2019-04-02 16:39:57 +0800

    优化构建，添加离线编译mysql

--- spark_2.2_hadoop_2.6.5/mysql.sh
+++ spark_2.2_hadoop_2.6.5/mysql.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-service mysqld start
+cd /usr/local/mysql-5.6.43/ && ./scripts/mysql_install_db --user=root
+service mysql start
 mysql -uroot mysql -e "source /usr/local/mysql_init.sql"
-service mysqld restart
+service mysql restart
