5aca288dbc883647f0ec507848cfe286857e0701
zck <chuankunzhang@mjitech.com>
2019-04-23 11:38:40 +0800

    6 ssh

---
+++ centso6.6_ssh_mysql/bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+service sshd start
+tail -f /dev/null
+CMD=${1:-"exit 0"}
+if [[ "$CMD" == "-d" ]];
+then
+	service sshd stop
+	/usr/sbin/sshd -D -d
+else
+	/bin/bash -c "$*"
+	echo "ok is me"
+fi
