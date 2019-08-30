851707276de0041b8a844cbc0e4439e98ee2212e
zck <zck573693104@gmail.com>
2019-08-30 20:21:17 +0800

    supperset

--- spark_2.2_hadoop_2.6.5/mysql.sh
+++ spark_2.2_hadoop_2.6.5/mysql.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /usr/local/mysql/ && ./scripts/mysql_install_db --user=root
 service mysql start
-mysql -uroot mysql -e "source /usr/local/mysql_init.sql"
+mysql -uroot mysql -e "source /usr/local/soft/mysql_init.sql"
 service mysql restart
